dde8dd799c50c76c14704b3159552f9f9b80f681
Kurniawan <kurnhyalcantara@gmail.com>
2023-03-26 17:44:23 +0800

    service, tokenize, validator for authentication part done. Move out to handler, postHandler done

---
+++ src/api/authentications/routes.js
@@ -0,0 +1,19 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/authentications',
+    handler: (req, h) => handler.postAuthenticationHandler(req, h),
+  },
+  {
+    method: 'PUT',
+    path: '/authentications',
+    handler: (req) => handler.putAuthenticationHandler(req),
+  },
+  {
+    method: 'DELETE',
+    path: '/authentications/',
+    handler: (req) => handler.deleteAuthenticationHandler(req),
+  },
+];
+
+module.exports = routes;
